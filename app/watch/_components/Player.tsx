e24311b0e5bcf01d9157086ceb578e634457b503
jasondev01 <jsnrbn01@gmail.com>
2024-03-13 13:16:51 +0800

    remove semi colons

--- app/watch/_components/Player.tsx
+++ app/watch/_components/Player.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import Artplayer from "artplayer";
-import { useEffect } from "react";
-import Hls from "hls.js";
-import { InfoInterface, SourceInterface, SourcesInterface } from "@/types";
+import Artplayer from "artplayer"
+import { useEffect } from "react"
+import Hls from "hls.js"
+import { InfoInterface, SourceInterface, SourcesInterface } from "@/types"
 
 type Props = {
     info: InfoInterface
@@ -19,16 +19,16 @@ export default function Player({ info, source }: Props) {
             customType: {
                 m3u8: function (video: any, url: string, art: any) {
                     if (Hls.isSupported()) {
-                        if (art.hls) art.hls.destroy();
-                        const hls = new Hls();
-                        hls.loadSource(url);
-                        hls.attachMedia(video);
-                        art.hls = hls;
-                        art.on("destroy", () => hls.destroy());
+                        if (art.hls) art.hls.destroy()
+                        const hls = new Hls()
+                        hls.loadSource(url)
+                        hls.attachMedia(video)
+                        art.hls = hls
+                        art.on("destroy", () => hls.destroy())
                     } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-                        video.src = url;
+                        video.src = url
                     } else {
-                        art.notice.show = "Unsupported playback format";
+                        art.notice.show = "Unsupported playback format"
                     }
                 },
             },
@@ -74,12 +74,12 @@ export default function Player({ info, source }: Props) {
                 column: 10,
             },
             icons: {},
-        });
+        })
 
         return () => {
             art.destroy()
-        };
-    }, [info, source]);
+        }
+    }, [info, source])
 
     return (
         <div className="artplayer-app h-[300px] md:h-[450px] xl:h-[650px]" />
